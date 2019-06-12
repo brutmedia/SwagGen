11b14809ddc2eb0ff47fcc2e10a0dff37b9a4bf5
Liquidsoul <liquidsoul@liquidsoul.fr>
2019-06-12 18:13:33 +0200

    Update version to 3.0.2b

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 TOOL_NAME = swaggen
-VERSION = 3.0.2a
+VERSION = 3.0.2b
 
 PREFIX = /usr/local
 INSTALL_PATH = $(PREFIX)/bin/$(TOOL_NAME)
--- Sources/SwagGen/main.swift
+++ Sources/SwagGen/main.swift
@@ -3,7 +3,7 @@ import PathKit
 import SwagGenKit
 import SwiftCLI
 
-let version = "3.0.2a"
+let version = "3.0.2b"
 let generateCommand = GenerateCommand()
 let cli = CLI(name: "swaggen", version: version, description: "Swagger code generator", commands: [generateCommand])
 cli.goAndExit()
